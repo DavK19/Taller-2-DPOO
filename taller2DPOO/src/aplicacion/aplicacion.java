dcce43b321dec8d635f6edef657e585dabbe28e5
DavK19 <jd.camargop1@uniandes.edu.co>
2023-08-26 14:21:14 -0500

    inicializado

---
+++ taller2DPOO/src/aplicacion/aplicacion.java
@@ -0,0 +1,41 @@
+package aplicacion;
+
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class aplicacion {
+	public void main(String[] args) {
+
+	}
+
+	public void mostrarMenu() {
+
+	}
+
+	/**
+	 * Este método sirve para imprimir un mensaje en la consola pidiéndole
+	 * información al usuario y luego leer lo que escriba el usuario.
+	 * 
+	 * @param mensaje El mensaje que se le mostrará al usuario
+	 * @return La cadena de caracteres que el usuario escriba como respuesta.
+	 */
+
+	public String input(String mensaje) {
+		try {
+			System.out.print(mensaje + ": ");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		} catch (IOException e) {
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
